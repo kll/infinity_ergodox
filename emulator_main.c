8a0e1b7e8c608e2f51a45ebf8091626d96e7e20a
Fred Sundvik <fsundvik@gmail.com>
2016-05-25 08:39:21 +0300

    Add led intensity rendering to emulator
    Also add some very basic profiling

--- emulator_main.c
+++ emulator_main.c
@@ -18,10 +18,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "gfx.h"
 #include "visualizer.h"
 #include <math.h>
+#include <stdio.h>
 
 static GDisplay* lcd;
 static GDisplay* led;
 static GDisplay* temp;
+static GDisplay* lightmap;
+static font_t font;
 
 GDisplay* get_lcd_display(void) {
     return lcd;
@@ -30,6 +33,36 @@ GDisplay* get_lcd_display(void) {
 GDisplay* get_led_display(void) {
     return led;
 }
+#define lightmap_size 128
+float min_intensity = 5.0f / 256.0f;
+
+static color_t intensity_map[256][lightmap_size][lightmap_size];
+
+float hue2rgb (float p, float q, float t){
+    if(t < 0.0f) t += 1.0f;
+    if(t > 1.0f) t -= 1.0f;
+    if(t < 1.0f/6.0f) return p + (q - p) * 6.0f * t;
+    if(t < 1.0f/2.0f) return q;
+    if(t < 2.0f/3.0f) return p + (q - p) * (2.0f/3.0f - t) * 6.0f;
+    return p;
+}
+
+color_t hslToRgb(float h, float s, float l){
+    float r, g, b;
+    h = h / 360.0f;
+
+    if(s == 0){
+        r = g = b = l; // achromatic
+    }else{
+        float q = l < 0.5f ? l * (1.0f + s) : l + s - l * s;
+        float p = 2.0f * l - q;
+        r = hue2rgb(p, q, h + 1.0f/3.0f);
+        g = hue2rgb(p, q, h);
+        b = hue2rgb(p, q, h - 1.0f/3.0f);
+    }
+    return RGB2COLOR((int)roundf(r * 255), (int)roundf(g * 255), (int)roundf(b * 255));
+}
+
 
 int main(void) {
 
@@ -37,6 +70,34 @@ int main(void) {
     lcd = gdispPixmapCreate(128, 32);
     led = gdispPixmapCreate(7, 7);
     temp = gdispPixmapCreate(GDISP_SCREEN_WIDTH, GDISP_SCREEN_HEIGHT);
+
+    lightmap = gdispPixmapCreate(lightmap_size * 8, lightmap_size * 8);
+
+    font = gdispOpenFont("DejaVuSansBold12");
+
+    const int half_lightmap = lightmap_size / 2;
+
+    float alpha = 0.001f;
+    float radius = lightmap_size / 2;
+    float beta = 1.0f / (radius * radius * min_intensity);
+    for(int i=0;i<256;i++) {
+        for (int j = 0;j < lightmap_size;j++) {
+            for (int k=0;k < lightmap_size;k++) {
+                float a = j - half_lightmap;
+                float b = k - half_lightmap;
+                float dist = a*a + b*b;
+                dist = sqrtf(dist);
+                float att = 1.0f / (1.0f + alpha*dist + beta*dist*dist);
+                float new_i = i * att;
+                color_t led_color = hslToRgb(200, 0.75f, new_i / 255.0f);
+                if (new_i < min_intensity * 255.0f) {
+                   led_color = RGB2COLOR(0, 0, 0);
+                }
+                intensity_map[i][j][k] = led_color;
+            }
+        }
+    }
+
     gdispSetDisplay(lcd);
 
     // Initialize and clear the display
@@ -56,9 +117,9 @@ typedef struct {
     point pos;
     float size;
     float rot;
-} key_t;
+} keyinfo_t;
 
-key_t keys[] = {
+keyinfo_t keys[] = {
      {{71.12f, 160.85f}, 1.5f, 0.0f},
      {{166.37f, 160.85f}, 1.0f, 0.0f},
      {{242.57f, 148.15f}, 1.0f, 0.0f},
@@ -116,32 +177,7 @@ key_t keys[] = {
      {{633.73f, 604.06f}, 1.0f, 65.0f},
 };
 
-static const int num_keys = sizeof(keys) / sizeof(key_t);
-
-float hue2rgb (float p, float q, float t){
-    if(t < 0.0f) t += 1.0f;
-    if(t > 1.0f) t -= 1.0f;
-    if(t < 1.0f/6.0f) return p + (q - p) * 6.0f * t;
-    if(t < 1.0f/2.0f) return q;
-    if(t < 2.0f/3.0f) return p + (q - p) * (2.0f/3.0f - t) * 6.0f;
-    return p;
-}
-
-color_t hslToRgb(float h, float s, float l){
-    float r, g, b;
-    h = h / 360.0f;
-
-    if(s == 0){
-        r = g = b = l; // achromatic
-    }else{
-        float q = l < 0.5f ? l * (1.0f + s) : l + s - l * s;
-        float p = 2.0f * l - q;
-        r = hue2rgb(p, q, h + 1.0f/3.0f);
-        g = hue2rgb(p, q, h);
-        b = hue2rgb(p, q, h - 1.0f/3.0f);
-    }
-    return RGB2COLOR((int)roundf(r * 255), (int)roundf(g * 255), (int)roundf(b * 255));
-}
+static const int num_keys = sizeof(keys) / sizeof(keyinfo_t);
 
 static inline int add_color_component(int a, int b) {
     int res = a + b;
@@ -156,6 +192,8 @@ static inline int add_colors(color_t a, color_t b) {
 }
 
 void draw_leds(int keyboard_x, int keyboard_y) {
+
+    //memset(final_intensity, 0, sizeof(final_intensity));
     for (int i=0;i<num_keys;i++) {
         if (keys[i].size == 0)
             continue;
@@ -166,18 +204,27 @@ void draw_leds(int keyboard_x, int keyboard_y) {
         point points[] = {
             {0, -20}
         };
-        gmiscMatrixFloat2DApplyToPoints(points, points, &rot, 5);
+        gmiscMatrixFloat2DApplyToPoints(points, points, &rot, 1);
 
         int row = i / 7;
         int col = i - row * 7;
         int luma = LUMA_OF(gdispGGetPixelColor(led, col, row));
         //color_t led_color = hslToRgb(200, 0.75f, 0.4f + 0.35f * luma / 255.0f);
-        color_t led_color = hslToRgb(200, 0.75f, 0.5f);
-        int x = points[0].x + mid_x;
-        int y = points[0].y + mid_y;
-        color_t src_color = gdispGetPixelColor(x, y);
-        color_t final_color = gdispBlendColor(led_color, src_color, luma);
-        gdispFillCircle(x, y, 50, final_color);
+        int led_x = points[0].x + mid_x;
+        int led_y = points[0].y + mid_y;
+
+        int start_x = led_x - lightmap_size / 2;
+        int start_y = led_y - lightmap_size / 2;
+        for (int a = 0; a < lightmap_size; ++a) {
+            int y = start_y + a;
+            for (int b = 0; b < lightmap_size; ++b) {
+                int x = start_x + b;
+                color_t src_color = gdispGetPixelColor(x, y);
+                color_t led_color = intensity_map[luma][a][b];
+                color_t final_color = add_colors(led_color, src_color);
+                gdispDrawPixel(x, y, final_color);
+            }
+        }
     }
 }
 
@@ -241,8 +288,6 @@ void draw_lcd(int keyboard_x, int keyboard_y) {
 }
 
 void draw_emulator(void) {
-    gdispSetDisplay(temp);
-    gdispClear(HTML2COLOR(0x8B4513));
     point points[] = {
         {25, 0},
         {210, 0},
@@ -264,17 +309,40 @@ void draw_emulator(void) {
     };
     int keyboard_x = 10;
     int keyboard_y = 10;
+    systemticks_t start_draw = gfxSystemTicks();
+
+    gdispSetDisplay(temp);
+    gdispClear(HTML2COLOR(0x8B4513));
 
     // Main keyboard area
     gdispFillConvexPoly(keyboard_x, keyboard_y, points, sizeof(points) / sizeof(point), HTML2COLOR(0xDADADA));
+    systemticks_t after_draw_keyboard = gfxSystemTicks();
 
-    draw_leds(keyboard_x, keyboard_y);
     draw_keycaps(keyboard_x, keyboard_y);
+    systemticks_t after_draw_keycaps = gfxSystemTicks();
+    draw_leds(keyboard_x, keyboard_y);
+    systemticks_t after_draw_leds = gfxSystemTicks();
     draw_lcd(keyboard_x, keyboard_y);
+    systemticks_t after_draw_lcd = gfxSystemTicks();
 
     gdispFlush();
+    systemticks_t after_flush = gfxSystemTicks();
+    systemticks_t total_time = after_flush - start_draw;
+    char buffer[256];
+    sprintf(buffer, "Frame time %i", total_time);
+    gdispDrawString(0, 700, buffer, font, White);
+    sprintf(buffer, "Keyboard %i, keycaps %i, leds %i, lcd %i",
+            after_draw_keyboard - start_draw,
+            after_draw_keycaps - after_draw_keyboard,
+            after_draw_leds - after_draw_keycaps,
+            after_draw_lcd - after_draw_leds
+            );
+    gdispDrawString(0, 715, buffer, font, White);
     gdispSetDisplay(gdispGetDisplay(0));
     gdispBlitArea(0, 0, GDISP_SCREEN_WIDTH, GDISP_SCREEN_HEIGHT, gdispPixmapGetBits(temp));
+    systemticks_t after_blit = gfxSystemTicks();
+    sprintf(buffer, "Frame time with flush %i", after_blit - start_draw);
+    gdispDrawString(0, 730, buffer, font, White);
     gdispFlush();
     gdispSetDisplay(lcd);
 }
